771c85f73a268d591fdbfc7fe1255c4a1a67d8f9
Phat <lvp3795@gmail.com>
2023-05-07 21:30:34 +0700

    fit about us

--- components/section/AboutUs.js
+++ components/section/AboutUs.js
@@ -22,7 +22,7 @@ function AboutUs() {
               }}
             ></div>
           </div>
-          <div className="flex-[6] px-8 py-14 w-[600px] max-sm:w-full max-sm:px-10 max-sm:py-6">
+          <div className="flex-[6] px-8 py-14 w-[600px] max-sm:w-full max-sm:px-16 max-sm:py-6">
             <div className="flex flex-col gap-6 text-justify tracking-wide leading-[1.5] relative">
               <div className="text-[35px] text-[#018489] font-bold z-10 font-sans max-sm:text-[30px]">
                 {t("common:about:title")}
@@ -39,7 +39,7 @@ function AboutUs() {
               <div className="w-[44.4px] h-[44.4px] bg-[#FFDE59] rounded-full absolute top-[-8px] left-[-31px] max-sm:top-[-15px]"></div>
             </div>
           </div>
-          <div className="py-14 max-sm:flex max-sm:justify-center max-sm:scale-90 max-sm:pt-0 max-sm:pb-5">
+          <div className="py-14 max-sm:flex max-sm:justify-center max-sm:scale-[0.8] max-sm:pt-0 max-sm:pb-5">
             <div className="w-[422px] h-[422px] border-[#0E8387] border-2 rounded-full ml-14 relative max-sm:ml-0">
               <div
                 className="w-[380px] h-[380px] rounded-full bg-white bg-cover absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-10 "
@@ -72,7 +72,7 @@ function AboutUs() {
         }}
       >
         <div className="relative max-sm:flex">
-          <div className="w-[579px] h-[714px] overflow-hidden max-sm:w-[384px] max-sm:h-[400px] max-sm:mr-5 max-sm:ml-2">
+          <div className="w-[579px] h-[714px] overflow-hidden max-sm:w-[354px] max-sm:h-[370px] max-sm:mr-7 max-sm:ml-0">
             <Image
               src="/assets/about-img1.webp"
               width={1790}
@@ -81,7 +81,7 @@ function AboutUs() {
               className="h-full w-full scale-y-[1.3] scale-x-[2.2]"
               style={{ objectPosition: "110px" }}
             />
-            <div className="absolute top-[-5px] left-[-5px] w-[79px] h-[62px] max-sm:left-[5px] max-sm:top-[-12px]">
+            <div className="absolute top-[-5px] left-[-5px] w-[79px] h-[62px] max-sm:left-[16px] max-sm:top-[-12px]">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="100%"
@@ -94,7 +94,7 @@ function AboutUs() {
                 <polygon points="105.42 9.92 105.42 0 0 0 0 105.42 9.92 105.42 9.92 9.92 105.42 9.92"></polygon>
               </svg>
             </div>
-            <div className="absolute bottom-[-7px] right-[-7px] w-[79px] h-[62px] rotate-180 max-sm:right-[7px] max-sm:bottom-[-11px]">
+            <div className="absolute bottom-[-7px] right-[-7px] w-[79px] h-[62px] rotate-180 max-sm:right-[15px] max-sm:bottom-[-11px]">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="100%"
@@ -112,11 +112,11 @@ function AboutUs() {
 
         <div className="w-[750px] justify-center flex flex-col gap-28 relative z-10 max-sm:w-full max-sm:pt-14">
           <div className="pl-12 w-[630px] flex flex-col gap-5 max-sm:w-fit max-sm:justify-center max-sm:pl-0 max-sm:mx-10">
-            <div className="text-[#ADFFFD] text-[38px] font-bold leading-[1.2] flex items-center gap-12 max-sm:text-[30px]">
+            <div className="text-[#ADFFFD] text-[38px] font-bold leading-[1.2] flex items-center gap-12 max-sm:text-[25px] max-sm:gap-7 max-sm:items-start">
               <div className="z-20">{t("common:about:vision:title")}</div>
-              <div className="w-[111px] h-[8px] bg-[#FFDE59]"></div>
+              <div className="w-[111px] h-[8px] bg-[#FFDE59] max-sm:w-[90px] max-sm:mt-2"></div>
             </div>
-            <div className="text-white text-[28px] leading-[1.4] text-justify max-sm:text-[18px]">
+            <div className="text-white text-[28px] leading-[1.4] text-justify max-sm:text-[15px]">
               {t("common:about:vision:content")}
             </div>
           </div>
@@ -129,11 +129,11 @@ function AboutUs() {
             }}
           >
             <div className="flex flex-col gap-3 max-sm:mx-10">
-              <div className="text-[#ADFFFD] text-right text-[35px] font-bold leading-[1.2] flex justify-end items-end gap-5 max-sm:flex-row-reverse max-sm:items-start">
-                <div className="w-[111px] h-[8px] bg-[#FFDE59] mb-2 max-sm:mt-3"></div>
+              <div className="text-[#ADFFFD] text-right text-[35px] font-bold leading-[1.2] flex justify-end items-end gap-5 max-sm:flex-row-reverse max-sm:items-start max-sm:text-[25px] max-sm:gap-7">
+                <div className="w-[111px] h-[8px] bg-[#FFDE59] mb-2 max-sm:mt-2"></div>
                 <div>{t("common:about:mission:title")}</div>
               </div>
-              <div className="text-white text-[18px] leading-[1.4] text-justify">
+              <div className="text-white text-[18px] leading-[1.4] text-justify max-sm:text-[15px]">
                 {t("common:about:mission:content")}
               </div>
             </div>

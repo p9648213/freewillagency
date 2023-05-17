7ffb4abae6ae219a9ea73f2dd2c237197a1b27c8
Phat <lvp3795@gmail.com>
2023-05-17 07:45:21 +0700

    remove duplicate

--- pages/_document.js
+++ pages/_document.js
@@ -5,15 +5,10 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link
           href="https://fonts.googleapis.com/css2?family=Niramit:wght@300;400;500&display=swap"
           rel="stylesheet"
         ></link>
-        <meta
-          http-equiv="Permissions-Policy"
-          content="accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()"
-        />
       </Head>
       <body>
         <Main />

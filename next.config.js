aa57b43161598da20f0e5a4a3ea2ccaf356a0785
Phat <lvp3795@gmail.com>
2023-05-17 08:09:59 +0700

    set request header

--- next.config.js
+++ next.config.js
@@ -5,4 +5,20 @@ const nextConfig = {
   i18n,
 };
 
-module.exports = nextConfig;
+module.exports = {
+  ...nextConfig,
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Permissions-Policy",
+            value:
+              "accelerometer=(),ambient-light-sensor=(),attribution-reporting=(),autoplay=(),battery=(),camera=(),clipboard-read=(),clipboard-write=(),conversion-measurement=(),cross-origin-isolated=(),direct-sockets=(),display-capture=(),document-domain=(),encrypted-media=(),execution-while-not-rendered=(),execution-while-out-of-viewport=(),focus-without-user-activation=(),fullscreen=(),gamepad=(),geolocation=(),gyroscope=(),hid=(),idle-detection=(),interest-cohort=(),magnetometer=(),microphone=(),midi=(),navigation-override=(),otp-credentials=(),payment=(),picture-in-picture=(),publickey-credentials-get=(),screen-wake-lock=(),serial=(),shared-autofill=(),speaker-selection=(),storage-access-api=(),sync-script=(),sync-xhr=(),trust-token-redemption=(),usb=(),vertical-scroll=(),wake-lock=(),web-share=(),window-placement=(),xr-spatial-tracking=()",
+          },
+        ],
+      },
+    ];
+  },
+};
